4f6f2f10266038bc62a06f4ee0f14646a75a1276
Wesley <wesleycamilo@gmail.com>
2022-10-25 19:36:02 -0300

    Initial Commit

---
+++ App do chucknorris/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App do chucknorris
+//
+//  Created by Wesley Camilo on 25/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
